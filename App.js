0b6f7dc05a01db70f369d3b8d4ed636a74b00689
MilouMohamed <49353852+MilouMohamed@users.noreply.github.com>
2023-05-28 15:24:08 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import App_tdl from './componentsRodMap/App_tdl'; 
+
+function App() {
+  return (
+    <div className='App'>
+      <br/><br/><hr/><br/><br/>
+       <App_tdl /> 
+    </div>
+  );
+}
+
+export default App;
